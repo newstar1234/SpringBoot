6a942ad951502e79d3f220c92658e71b470f8bcd
newstar <maori36@naver.com>
2023-08-30 00:06:28 +0900

    23.08.29

---
+++ ex00/src/main/java/com/example/ex00/Computer.java
@@ -0,0 +1,11 @@
+package com.example.ex00;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Computer {
+  private int ram;
+}
---
+++ ex00/src/test/java/com/example/ex00/dependency/ComputerTests.java
@@ -0,0 +1,23 @@
+package com.example.ex00.dependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.ex00.Coding;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+public class ComputerTests {
+  // 단위 테스트에서는 필드 주입만 사용 가능하다.
+  @Autowired
+  Coding coding;
+  
+  @Test
+  public void computerTest(){
+    // 메소드 안에서 @Autowired를 사용할 수 없다.
+  log.info(coding.getComputer().toString());
+  }
+}
